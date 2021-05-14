ce496696a79b262c15bc0d13fad2f06e41678c8c
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-05-14 11:08:28 +0530

    Added 3 elements in a list

---
+++ src/main/java/MyNode.java
@@ -0,0 +1,17 @@
+public class MyNode <K> {
+    private K key;
+    private MyNode<K> next;
+
+    public MyNode(K key) {
+        this.key = null;
+        this.next = null;
+    }
+
+    public void setNext(MyNode<K> next) {
+        this.next = next;
+    }
+
+    public MyNode<K> getNext() {
+        return next;
+    }
+}
