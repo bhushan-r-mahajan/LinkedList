0d060eb9213c939f06ba87a1d66dc08df6b947d7
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-05-15 12:11:45 +0530

    Implemented push, peek methods for queue

---
+++ src/main/java/Queue.java
@@ -0,0 +1,24 @@
+public class Queue<K> {
+    private final LinkedList<K> linkedList;
+
+    public Queue() {
+        linkedList = new LinkedList<>();
+    }
+
+    public void push(LNode value) {
+        linkedList.append(value);
+    }
+
+    public void printStack() {
+        linkedList.printList();
+    }
+
+    public void size() {
+        linkedList.getSize();
+    }
+
+    public LNode peek() {
+        System.out.println("The Element after peeking is: " + linkedList.head.getKey());
+        return linkedList.head;
+    }
+}
